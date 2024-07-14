29b0b7e50bf332e79dc42ce0ea4552f4d1f1db1b
Amar <amarnath.arjsd@gmail.com>
2024-07-14 12:23:33 +0530

    updated_code

---
+++ 4.QA_System_With_LlamaIndex‬_and_GoogleGemini_Using_LlamaIndex_GeminiEmbedding_GeminiPro/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication_Using_Gemini',
+    version= '0.0.1',
+    author= 'amarnath',
+    author_email= 'amarnath.arjsd@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
