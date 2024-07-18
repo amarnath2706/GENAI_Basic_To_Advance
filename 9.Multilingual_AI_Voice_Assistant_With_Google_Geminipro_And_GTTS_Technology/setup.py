5fe0f0cfab9e1caef65cbe27d5e3e135b9ed3971
Amar <amarnath.arjsd@gmail.com>
2024-07-18 20:05:54 +0530

    updated_code

---
+++ 9.Multilingual_AI_Voice_Assistant_With_Google_Geminipro_And_GTTS_Technology/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Multilingual AI Voice Assistant',
+    version= '0.0.1',
+    author= 'amarnath',
+    author_email= 'amarnath.arjsd@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
