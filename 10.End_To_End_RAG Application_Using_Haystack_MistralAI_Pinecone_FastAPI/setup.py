15f3a203dc74c71e575d51c132852d1afad34603
Amar <amarnath.arjsd@gmail.com>
2024-07-20 11:22:39 +0530

    updated_code

---
+++ 10.End_To_End_RAG Application_Using_Haystack_MistralAI_Pinecone_FastAPI/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'RAG_using_haystack_MistralAI_pinecone_fastapi',
+    version= '0.0.1',
+    author= 'amarnath',
+    author_email= 'amarnath.arjsd@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
