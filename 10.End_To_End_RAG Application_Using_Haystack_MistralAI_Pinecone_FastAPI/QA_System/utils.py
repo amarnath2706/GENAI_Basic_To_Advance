aac4dceff524edbf3fcc4155acca82561f93c021
Amar <amarnath.arjsd@gmail.com>
2024-07-20 11:56:13 +0530

    updated_code

--- 10.End_To_End_RAG Application_Using_Haystack_MistralAI_Pinecone_FastAPI/QA_System/utils.py
+++ 10.End_To_End_RAG Application_Using_Haystack_MistralAI_Pinecone_FastAPI/QA_System/utils.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+HF_TOKEN = os.getenv('HF_TOKEN')
 os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
+os.environ['HF_API_TOKEN'] = HF_TOKEN
     
 print("Import Successfully")
 
